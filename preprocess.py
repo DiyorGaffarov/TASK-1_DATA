046f83d36052b1cddd15325a8d541187e2e23e31
Diyor Gaffarov <diyorgaffarov@gmail.com>
2026-04-05 21:55:26 +0500

    chore: json processed

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,17 @@ import json
 with open("dataset/task1_d.json", "r") as f:
     data = f.read()
 
-# Convert Ruby-style → JSON
-data = re.sub(r":(\w+)=>", r'"\1":', data)   # keys
-data = data.replace("'", '"')                # quotes
+data = re.sub(r":(\w+)=>", r'"\1":', data)
+
+data = re.sub(r'(?<![a-zA-Z]):\s*([a-zA-Z_]\w*)', r'"\1"', data)
+
+data = data.replace("\\'", "'")
 
 books = json.loads(data)
+print(f"Successfully loaded {len(books)} records")
+
+with open("dataset/task1_d_processed.json", "w", encoding="utf-8") as f:
+    json.dump(books, f, indent=4, ensure_ascii=False)
+
+print("Saved to dataset/task1_d_processed.json")
+
